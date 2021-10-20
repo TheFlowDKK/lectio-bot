59e21c748bc70ab7ad35233b3e568f94bb38f863
TheFlowDKK <oliver@ihd.dk>
2021-10-20 23:10:25 +0200

    Not mandatory cronjob script

---
+++ cronjob.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# This file is not mandatory.
+# It's just a file your cronjob can run to update your database.
+
+import sql_database as sqldb
+
+sqldb.uploadToTable()
+sqldb.deleteOld()
