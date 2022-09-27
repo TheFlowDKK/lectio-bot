ca99d4bf572d6dbf613496d218f3762d6d601ce1
TheFlowDKK <oliver@ihd.dk>
2022-09-27 22:48:54 +0200

    Log update

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 import os
 from dotenv import load_dotenv
+from datetime import datetime
 
 import sql_database as sqldb
 
@@ -10,6 +11,21 @@ TOKEN = os.getenv('BOT_TOKEN')
 GUILD = os.getenv('GUILD_NAME')
 OWNER = os.getenv('OWNER_ID')
 
+logDir = f'{os.getcwd()}/logs'
+
+def log(*log, user = "", startup = False):
+    date = format(datetime.now(), "%d-%m-%Y")
+    if startup:
+        os.system(f"echo '\\n' >> {logDir}/{date}.txt")
+    time = format(datetime.now(), "%d-%m-%Y %H:%M")
+    print(time)
+    os.system(f"echo '{time}' >> {logDir}/{date}.txt")
+    for i in range(len(log)):
+        print(log[i])
+        if user == "":
+            os.system(f"echo '{log[i]}' >> {logDir}/{date}.txt")
+        else:
+            os.system(f"echo '{log[i]}  ---  User: {user}' >> {logDir}/{date}.txt")
 
 client = discord.Client()
 
@@ -24,7 +40,7 @@ def makeEmbed(titel, navn, frist, elevtid):
     embed.set_author(name="TheFlowDK", url="https://steamcommunity.com/id/TheFlowDKK/", icon_url="https://cdn.discordapp.com/avatars/245792114745540610/88447856c0f0e1505d00bab466fc5467.png?size=128")
     return embed
 
-
+log(" - Bot startet - ", f'Servere: {GUILD}',f'Admins: {OWNER}', startup=True)
 
 @client.event
 async def on_ready():
@@ -33,15 +49,10 @@ async def on_ready():
         if guild.name == GUILD:
             break
     if guild.name != GUILD:
-        print("Noget gik galt...")
-        print("Din .env fil er muligvis ikke opsat korrekt.")
-        input()
+        log("Noget gik galt...", "Din .env fil er muligvis ikke opsat korrekt.")
         exit()
     
-    print(
-        f"{client.user} aktiv! (User-ID: {client.user.id}) \n"
-        f"Guild: {guild.name} (Guild-ID: {guild.id})"
-        )
+    log(f"{client.user} aktiv! (User-ID: {client.user.id})", f"Guild: {guild.name} (Guild-ID: {guild.id})")
 
 
 @client.event
@@ -55,6 +66,7 @@ async def on_message(message):
         except:
             lektier = sqldb.showNext(1)
             embed = makeEmbed("Næste lektie", f'{lektier[0][0]}', f'{lektier[0][1].strftime("%H:%M %d/%m/%Y")}', f'{lektier[0][2]}')
+            log(f'{message.content}', 'Næste lektie', user=message.author)
             await message.channel.send(embed=embed)
             return
 
@@ -72,17 +84,21 @@ async def on_message(message):
             elevtid += f'{lektier[x][2]}\n'
             #besked += (f'{lektier[x][0]} {lektier[x][1].strftime("%H:%M %d/%m/%Y")} {lektier[x][2]}\n')
         embed = makeEmbed(f"Næste {len(lektier)} lektier", navn, frist, elevtid)
+        log(f'{message.content}', f'Næste {len(lektier)} lektier', user=message.author)
         await message.channel.send(embed=embed)
     
 
     if message.content.startswith('!updatedb') and message.author.id == int(OWNER):
         sqldb.uploadToTable()
         sqldb.deleteOld()
+        log(f'!updatedb', f'Database opdateret...', user=message.author)
         await message.channel.send(f'Database opdateret...')
     elif message.content.startswith('!updatedb') and message.author.id != int(OWNER):
+        log(f'Mangler tilladelse til !updatedb', user=message.author)
         await message.channel.send(f'Mangler tilladelse...')
 
     if message.content.startswith('!help'):
+        log("!help", user=message.author)
         await message.channel.send(
         '!lektier - Viser den næste lektie\n' + '!lektier [antal] - Viser det næste antal lektier\n'
         )
